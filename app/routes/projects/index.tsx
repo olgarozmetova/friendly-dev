9bf6141ebdf51c7b60e3dad97e1951a54e559e30
olga rozmetova <olga.rozmetova@gmail.com>
2026-08-18 19:50:51 +0200

    add pagination to project page

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react'
 import type { Route } from './+types/index'
 import type { Project } from '~/types'
 import ProjectCard from '~/components/ProjectCard'
+import Pagination from '~/components/Pagination'
 
 export async function loader({
   request,
@@ -13,14 +15,30 @@ export async function loader({
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
   const { projects } = loaderData as { projects: Project[] }
 
+  const [currentPage, setCurrentPage] = useState(1)
+  const projectsPerPage = 10
+
+  // Caclulate total pages
+  const totalPages = Math.ceil(projects.length / projectsPerPage)
+
+  // Get current pages projects
+  const indexOfLast = currentPage * projectsPerPage
+  const indexOfFirst = indexOfLast - projectsPerPage
+  const currentProjects = projects.slice(indexOfFirst, indexOfLast)
+
   return (
     <>
       <h2 className="text-3xl text-white text-bold mb-8">🚀 Projects</h2>
       <div className="grid gap-6 sm:grid-cols-2">
-        {projects.map(project => (
+        {currentProjects.map(project => (
           <ProjectCard key={project.id} project={project} />
         ))}
       </div>
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        onPageChange={setCurrentPage}
+      />
     </>
   )
 }
